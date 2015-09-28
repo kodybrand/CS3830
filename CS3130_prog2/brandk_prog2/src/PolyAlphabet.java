f1da96705a5403b3a481100a16d5690e3d48355a
Kody Brand <kodybrand@gmail.com>
2015-09-28 12:16:12 -0500

    Created a new class for encryption --kbrand

---
+++ CS3130_prog2/brandk_prog2/src/PolyAlphabet.java
@@ -0,0 +1,19 @@
+
+/**
+
+ @author Kody
+ */
+public class PolyAlphabet
+{
+
+   public PolyAlphabet()
+   {   
+   }
+   
+   public String encode(String letter, int shift) 
+   {
+      
+      return null;
+   }
+
+}
